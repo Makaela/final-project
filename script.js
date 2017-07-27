adcb053ba80c049c40233042a2128f2545b3f82a
Makaela <MartinezM2019@365.bcsd.org>
2017-07-27 15:22:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,10 @@ function openCity(cityName, elmnt, color) {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
-
+ function speech2() {
+   document.getElementById("mytext12").style.display='none';
+   document.getElementById("christa").innerHTML = 'Thank you for posting a comment!'
+ }
 function speech() {
   document.getElementById("mytext").style.display='none';
   document.getElementById("mytext2").style.display='none';
@@ -38,4 +41,31 @@ function speech1() {
   document.getElementById("mytext10").style.display='none';
   document.getElementById("mytext11").style.display='none';
   document.getElementById("brianna").innerHTML = "We will contact you about your volunteer work!";
+}
+
+function speech3() {
+ document.getElementById("mytext13").style.display='none';
+ document.getElementById("mytext14").style.display='none';
+document.getElementById("ori").style.display='none';
+
+ var username = document.getElementById("mytext13").value;
+ var password = document.getElementById("mytext14").value;
+ if (username=="puppylover333"&&password=="makaela") {
+    document.getElementById("annie").innerHTML = "You are now logged in!";
+    document.getElementById("ori").innerHTML = "Welcome, puppylover333";
+    document.getElementById("ori").style.display='block';
+ }
+
+ else {
+       document.getElementById("annie").innerHTML = "User not found";
+ }
+}
+
+function speech4() {
+ document.getElementById("mytext15").style.display='none';
+ document.getElementById("mytext16").style.display='none';
+ document.getElementById("mytext17").style.display='none';
+ document.getElementById("mytext18").style.display='none';
+  document.getElementById("mytext19").style.display='none';
+ document.getElementById("aby").innerHTML = "Thank you for signing up!";
 }
